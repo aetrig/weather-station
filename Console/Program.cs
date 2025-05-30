7e0b5f8a1e73b222424f0d731f6780bc2c4f6c37
aetrig <emilia.gosztyla@gmail.com>
2025-05-30 19:35:45 +0200

    getting data from MQTT + DB working

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,4 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Threading.Tasks;
+using InfluxDB3.Client;
+using InfluxDB3.Client.Config;
+using InfluxDB3.Client.Write;
+
+
+// // See https://aka.ms/new-console-template for more information
 using System.Text;
 using MQTTnet;
 using MQTTnet.Client;
@@ -6,18 +12,27 @@ using MQTTnet.Client.Options;
 using MQTTnet.Client.Subscribing;
 using dotenv.net;
 
+
+//Getting data from .env
 var root = Directory.GetCurrentDirectory();
 Console.WriteLine(root);
 var dotenv = Path.Combine(root.Replace(@"bin\Debug\net8.0", ""), ".env");
 Console.WriteLine(dotenv);
-//DotEnv.Load(options: new DotEnvOptions(envFilePaths: new[] { dotenv.ToString() }));
 var envs = DotEnv.Read(options: new DotEnvOptions(envFilePaths: new[] { dotenv.ToString() }));
 var keys = envs.Keys;
 
+//Setting up database connection
+const string host = "http://localhost:8181";
+const string token = "apiv3_6H81no4cbN6wfeDlzqwnnCPKnyfrwNF4kbRL0nHU_df-q-j8MIjdObqen2viebVuYBAX5N9JRyZivF6mqPXokg";
+const string database = "weather-station";
+
+using var InfluxDBclient = new InfluxDBClient($"{host}?token={token}&database={database}");
+
+//Setting up MQTT Broker connection
 string broker = envs["BROKER_IP"];
 int port = 1883;
 string clientId = "mqtt-explorer-6c1ebc07";
-string topic = "test_topic";
+string topic = "weather/#";
 
 var factory = new MqttFactory();
 var mqttClient = factory.CreateMqttClient();
@@ -34,6 +49,8 @@ var subscribingOptions = new MqttClientSubscribeOptionsBuilder()
 
 var connectResult = await mqttClient.ConnectAsync(options, CancellationToken.None);
 
+
+//Reading data from MQTT Broker
 if (connectResult.ResultCode == MQTTnet.Client.Connecting.MqttClientConnectResultCode.Success)
 {
 	Console.WriteLine("Connected to MQTT broker successfully.");
@@ -42,10 +59,28 @@ if (connectResult.ResultCode == MQTTnet.Client.Connecting.MqttClientConnectResul
 	await mqttClient.SubscribeAsync(subscribingOptions, CancellationToken.None);
 	while (true)
 	{
-		mqttClient.UseApplicationMessageReceivedHandler(e =>
+		mqttClient.UseApplicationMessageReceivedHandler(async e =>
 		{
-			Console.WriteLine($"Received message: {Encoding.UTF8.GetString(e.ApplicationMessage.Payload)}");
+			string topic = e.ApplicationMessage.Topic;
+			string payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+			string[] splitTopic = topic.Split("/");
+
+			var point = PointData.Measurement("weather").SetTag("sensor", splitTopic[1]).SetField(splitTopic[2], payload).SetTimestamp(DateTime.UtcNow);
+			await InfluxDBclient.WritePointAsync(point: point);
+
+
+			Console.WriteLine($"Message received -> Full Topic: {topic}\nSensor Name: {splitTopic[1]}\nMeasurement: {splitTopic[2]} Value: {payload}");
 		});
 	}
-	// Callback function when a message is received
 }
+
+
+
+
+// const string sql = "SELECT sensor,temperature,humidity,pressure FROM weather";
+// Console.WriteLine("{0,-30}{1,-15}{2,-15}{3,-15}", "sensor", "temperature", "humidity", "pressure");
+// await foreach (var row in client.Query(query: sql))
+// {
+// 	Console.WriteLine("{0,-30}{1,-15}{2,-15}{3,-15}", row[0], row[1], row[2], row[3]);
+// }
+// Console.WriteLine();
